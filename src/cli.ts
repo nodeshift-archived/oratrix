0a67508c04bd2fb21fad001d35a439493f2f5b72
Helio Frota <00hf11@gmail.com>
2020-09-24 08:35:28 -0300

    src: fix lint issue with options (#21)
    * src: fix lint issue
    
    * src: fix warning  '_' is defined but never used
    
    * src: suppress the any type issue from license-checker

--- src/cli.ts
+++ src/cli.ts
@@ -1,14 +1,13 @@
 #!/usr/bin/env node
 
 import yargs from 'yargs';
-import * as option from './option';
 
-// Initialize the options.
-const options = {
-  cwd: option.cwd(),
-  fields: option.fields(),
-  organization: option.organization(),
-};
-
-yargs.commandDir('commands').demandCommand(1).options(options).argv;
+yargs.commandDir('commands').demandCommand(1).options({
+  cwd: {
+    type: 'string',
+    default: process.cwd(),
+    required: false,
+    description: 'The current working directory',
+  },
+}).argv;
  
