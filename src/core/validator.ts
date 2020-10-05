91bb47a1f58c2b0fe6fc1ca44ad771ea500a5b7b
alexalikiotis <alexalikiotis5@gmail.com>
2020-10-05 12:17:00 +0300

    adding options object

--- src/core/validator.ts
+++ src/core/validator.ts
@@ -2,26 +2,33 @@ import FieldLoader from '../util/fieldLoader';
 import Differ from '../util/differ';
 import GithubFetcher from '../util/githubFetcher';
 
+export interface Options {
+  config?: string;
+}
+
 class Validator {
   fieldLoader = new FieldLoader();
   differ = new Differ();
 
-  async run(organization?: string): Promise<string[]> {
+  async run(organization?: string, options: Options = {}): Promise<string[]> {
     if (organization) {
-      return this.runOrganizationCheck(organization);
+      return this.runOrganizationCheck(organization, options);
     } else {
-      return this.runLocalCheck();
+      return this.runLocalCheck(options);
     }
   }
 
-  async runLocalCheck(): Promise<string[]> {
-    const requiredFields = await this.fieldLoader.loadFields();
+  async runLocalCheck(options: Options): Promise<string[]> {
+    const requiredFields = await this.fieldLoader.loadFields(options.config);
     const packageFields = await this.fieldLoader.loadFields('./package.json');
     const report = this.differ.run(requiredFields, packageFields);
     return report;
   }
 
-  async runOrganizationCheck(organization: string): Promise<string[]> {
+  async runOrganizationCheck(
+    organization: string,
+    options: Options
+  ): Promise<string[]> {
     const ghFetcher = new GithubFetcher();
     const result = await ghFetcher.fetch(organization);
     return result;
