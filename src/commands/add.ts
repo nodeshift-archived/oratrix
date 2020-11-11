78082ddfffd950d10032ddec200a5cd8c2b5af33
Helio Frota <00hf11@gmail.com>
2020-11-11 09:06:08 -0300

    chore: add missing prompts dependency (#73)

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -1,52 +1,51 @@
-import { existsSync } from "fs"
-import { readFileSync } from "fs"
-import { writeFileSync } from "fs"
-import { PackageFormat } from "../config/PackageFormat"
-import { getPathToPackages } from "../util/fileHelpers"
+import { existsSync } from 'fs';
+import { readFileSync } from 'fs';
+import { writeFileSync } from 'fs';
+import { PackageFormat } from '../config/PackageFormat';
+import { getPathToPackages } from '../util/fileHelpers';
 
-const prompts = require('prompts');
+import prompts from 'prompts';
 
+export const command = 'add <package>';
 
-export const command = 'add <package>'
-
-export const desc = 'Add new package to `.packages.json` file'
+export const desc = 'Add new package to `.packages.json` file';
 
 export const builder = (args: any) => {
   args.positional('package', {
     describe: 'Package name',
-    type: 'string'
+    type: 'string',
   });
 };
-
 
 export async function handler(params: any) {
   const name = params.package;
   const pathToFile = getPathToPackages();
   if (!existsSync(pathToFile)) {
-    console.log("Current location is missing packages file")
+    console.log('Current location is missing packages file');
     return;
   }
 
   if (!name) {
-    console.log("Name should be provided provided")
+    console.log('Name should be provided provided');
     return;
   }
 
-  const file = readFileSync(pathToFile, "utf8")
+  const file = readFileSync(pathToFile, 'utf8');
   const packageContent: PackageFormat = JSON.parse(file);
 
-  const response = await prompts([{
-    type: 'text',
-    name: 'category',
-    message: 'Category for your package?'
-  }]);
+  const response = await prompts([
+    {
+      type: 'text',
+      name: 'category',
+      message: 'Category for your package?',
+    },
+  ]);
 
   if (packageContent.packages) {
     packageContent.packages[name] = {
-      ...response
-    }
+      ...response,
+    };
     writeFileSync(pathToFile, JSON.stringify(packageContent, null, 2));
-    console.log(`Successfully initialized packages file`)
+    console.log(`Successfully initialized packages file`);
   }
 }
-
