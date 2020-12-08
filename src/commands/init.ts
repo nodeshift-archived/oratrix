e2520d040ba30353e322897653a7d9bbf998c3fd
Alex Alikiotis <alexalikiotis5@gmail.com>
2020-12-08 12:21:45 +0200

    refactoring

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,6 +1,5 @@
 import fs from 'fs';
 import path from 'path';
-import { js as beautify } from 'js-beautify';
 
 export const command = 'init';
 
@@ -10,22 +9,23 @@ export const builder = {};
 
 export const handler = (): void => {
   // get full path
-  const filepath = path.resolve('oratrix.config.js');
+  const filepath = path.resolve('.oratrixrc.json');
   const exists = fs.existsSync(filepath);
   // check if a oratrix config file already exists
   if (exists) {
     console.log('❗️ config file already exists in your directory.');
     return;
   }
-  // config contents as string
-  const contents =
-    'module.exports = {\nversion: 1,\n// required: [],\n// allowed: {},\n// packages: []\n};';
-  // format config before write it to file
-  const config = beautify(contents, {
-    indent_size: 2,
-    space_in_empty_paren: true,
-  });
+  // config contents as js-object
+  const initConfig = {
+    version: 1,
+    required: [],
+    allowed: {},
+    packages: [],
+  };
 
-  fs.writeFileSync(filepath, config, { encoding: 'utf-8' });
+  fs.writeFileSync(filepath, JSON.stringify(initConfig, null, 2), {
+    encoding: 'utf-8',
+  });
   console.log('✨ config file successfully initialized.');
 };
